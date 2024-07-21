411e557d6072f31cdc498af70565e6a45a2d1c58
Rajan-Jasani9 <rajan.jasani2002@gmail.com>
2024-07-21 22:52:46 +0530

    feat: develop cut operation and recursive code for copy operations for folders.

--- document_structure/views.py
+++ document_structure/views.py
@@ -79,3 +79,81 @@ class CreateFolderView(GenericAPIView):
             },
             status=status.HTTP_201_CREATED
         )
+    
+
+class CutCopyOperationPasteView(GenericAPIView):
+
+    def post(self, request):
+        '''
+        Example Payload:
+            {
+                'source_folder/document_id': id,
+                'destination_folder': id,
+                'operation': 'cut'/'copy',
+            }
+        '''
+        source_folders = request.data.get('source_folders', [])
+        source_documents = request.data.get('source_documents', [])
+        operation = request.data.get('operation',None)
+        destination_folder = request.data.get('destination_folder', None)
+
+        if not operation:
+            return Response(
+                data={
+                    'message':'API Payload not accurate!!',
+                    'status':status.HTTP_400_BAD_REQUEST,
+                    'data': {}
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        destination_folder = Folder.objects.filter(id=destination_folder, is_delete=False).first() if destination_folder else None
+
+        if operation == 'cut':
+            for folder in source_folders:
+                folder = Folder.objects.filter(id=folder, is_delete=False).first()
+                folder.parent = destination_folder
+                folder.save()
+
+            for document in source_documents:
+                document = Document.objects.filter(id=document, is_delete=False).first()
+                document.library_folder = destination_folder 
+                document.save()
+
+        if operation == 'copy':
+            # keeping pending till decision of database is decided for documents.
+            pass
+
+        return Response(
+            data={
+                'message': f'{operation.capitalize()} operation performed successfully',
+                'status':status.HTTP_200_OK,
+                'data': {}
+            },
+            status=status.HTTP_200_OK
+        )
+
+
+
+## Helper Functions section:
+
+def copy_sub_folders(folder, new_folder):
+    '''
+    Recursive function to copy all subfolders:  
+    '''
+    sub_folders = Folder.objects.filter(parent=folder, is_delete=False)
+
+    if sub_folders.count() == 0:
+        return True
+
+    for sub_folder in sub_folders:
+        new_copied_folder = Folder.objects.create(parent=new_folder)
+        copy_sub_folders(sub_folder, new_copied_folder)
+
+    return True
+
+def copy_sub_documents(folder, new_folder):
+
+    documents=Document.objects.filter(library_folder=folder, is_delete=False)
+    # keeping pending based on decision on where to set up database as code will differ accordingly!!
+    pass
